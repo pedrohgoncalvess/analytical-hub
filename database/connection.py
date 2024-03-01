5b0fa0d63e9e0701eee09989419befc1c48c9ab2
unknown <pedro_gonsalves@hotmail.com>
2024-03-01 16:43:23 -0300

    add: string to establish connection

--- database/connection.py
+++ database/connection.py
@@ -1,10 +1,37 @@
+import sqlalchemy
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker, Session
+from starlette import status
 from utils.configs import get_env_var
+from utils.local_exception import LocalException
 
 
 class DatabaseConnection:
     def __init__(self):
-        self.dbHost = get_env_var("DB_HOST")
-        self.dbPort = get_env_var("DB_PORT")
-        self.dbName = get_env_var("DB_NAME")
-        self.dbUser = get_env_var("DB_USER")
-        self.dbPassword = get_env_var("DB_PASSWORD")
+        self.__dbHost__ = get_env_var("DB_HOST")
+        self.__dbPort__ = get_env_var("DB_PORT")
+        self.__dbName_ = get_env_var("DB_NAME")
+        self.__dbUser__ = get_env_var("DB_USER")
+        self.__dbPassword__ = get_env_var("DB_PASSWORD")
+
+        self._engine_ = create_engine(
+            f"postgresql+psycopg2://{self.__dbUser__}:{self.__dbPassword__}@{self.__dbHost__}:{self.__dbPort__}/{self.__dbName_}")
+        self.schemas = ['file']
+
+    def __newSession__(self) -> Session:
+        return sessionmaker(autocommit=False, autoflush=False, bind=self._engine_, expire_on_commit=False)()
+
+    def addNewObject(self, newObject):
+        session = self.__newSession__()
+        global valueReturn
+        try:
+            session.add(newObject)
+            session.commit()
+            valueReturn = True
+        except sqlalchemy.exc.IntegrityError:
+            valueReturn = LocalException("Duplicate object", status_code=status.HTTP_409_CONFLICT)
+        except Exception:
+            valueReturn = LocalException()
+        finally:
+            session.close()
+            return valueReturn
