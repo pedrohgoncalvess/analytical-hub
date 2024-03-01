78a73e8a747690b4b3a9d817c723a5b8d9b4e922
unknown <pedro_gonsalves@hotmail.com>
2024-03-01 18:25:57 -0300

    add: host so can access the dockerize api locally

--- main.py
+++ main.py
@@ -90,4 +90,4 @@ async def get_file_list():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=9000, log_level="info", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
